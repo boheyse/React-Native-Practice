a920934fdf49f3088e59d231e496b9e3c60dd7a0
BHeyse <dbh47@txstate.edu>
2020-05-10 12:06:40 -0500

    TextInput box and Add button for course goals

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I want to see if this works</Text>
+    <View style={{padding:50}}>
+      <View>
+        <TextInput
+          placeholder="Course Goals"
+          style={{borderColor:'black', borderWidth: 1, padding:10}}
+          />
+        <Button title="Add"/>
+      </View>
+      <View>
+      </View>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
